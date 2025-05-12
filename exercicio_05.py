29a2ac5f593e9d5b487ee051f8738a93254d456b
CAIO761 <00001111146214sp@al.educacao.sp.gov.br>
2025-05-12 14:18:15 -0300

    nota do bimestre

--- exercicio_05.py
+++ exercicio_05.py
@@ -1,13 +1,21 @@
-def adicionar_aluno(nome, email, serie):
+from exercicio_04 import somar_pares                                          
+
+def adicionar_aluno(nome, email, serie, nota1= 0, nota2= 0, nota3= 0):
+    notas = [nota1, nota2, nota3]
     lista_alunos = []
     aluno = {
         "nome": nome,
         "email": email,
         "serie": serie,
-        "notas": []
+        "notas": [nota1, nota2, nota3],
+        "media": somar_pares(notas)
     }
     lista_alunos.append(aluno)
     return lista_alunos
 
 
-print(adicionar_aluno("caio", "caio@g.com", "2TB"))
+
+print(adicionar_aluno("caio", "caio@g.com", "2TB", 5, 7, 8))
+
+#from = quer dizer que ele vai pegar uma funcao de outro arquivo
+#toda funcao precisa de parenteses me frente dela
